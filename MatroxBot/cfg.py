2827dfb6884f5c904962a84654003c653edf1ea4
matroxkod <ayounga1@gmail.com>
2017-10-08 19:43:27 -0700

    Updated deleted cfg

---
+++ MatroxBot/cfg.py
@@ -0,0 +1,8 @@
+# cfg.py
+import secrets
+HOST = "irc.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = secrets.NICK                 # your Twitch username, lowercase
+PASS = secrets.PASS                 # your Twitch OAuth token
+CHAN = secrets.CHAN                 # the channel you want to join
+RATE = (20/30)                      # messages per second 
