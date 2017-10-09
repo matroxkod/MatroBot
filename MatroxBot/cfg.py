fe8ab3553542d3ffa2fa1e45385defacc9ff05ae
matroxkod <ayounga1@gmail.com>
2017-10-08 17:36:35 -0700

    Moved sensitive info to secrets file
    Moved to secrets file.

--- MatroxBot/cfg.py
+++ MatroxBot/cfg.py
@@ -1,7 +1,14 @@
 # cfg.py
+import secrets
+
 HOST = "irc.twitch.tv"              # the Twitch IRC server
 PORT = 6667                         # always use port 6667!
-NICK = "matroxspotbot"            # your Twitch username, lowercase
-PASS = "oauth:qzy8xp59sggzyego0mz5xq920t157g" # your Twitch OAuth token
-CHAN = "#matroxkod"                   # the channel you want to join
+NICK = secrets.NICK                 # your Twitch username, lowercase
+PASS = secrets.PASS                 # your Twitch OAuth token
+CHAN = secrets.CHAN                 # the channel you want to join
 RATE = (20/30) # messages per second
+
+# Secrets file should be of the following format:
+# NICK = "NAMEOFBOT"
+# PASS = "OATH:KEY"
+# CHAN = "#YOUCHANNELNAME" 
