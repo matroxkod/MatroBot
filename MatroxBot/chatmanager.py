d6625081c4ae5cda2b8bd50bc49822810529d143
matroxkod <ayounga1@gmail.com>
2017-10-18 10:19:50 -0700

    Comments and formatting

--- MatroxBot/chatmanager.py
+++ MatroxBot/chatmanager.py
@@ -1,9 +1,9 @@
-#chatmanager.py
+# chatmanager.py
 from singleton import Singleton
 import cfg
 import socket
 
-# This class is designed to by the point of entry for all chat actions
+# This class is designed to be the point of entry for all chat actions
 @Singleton
 class ChatManager:
     def __init__(self):
@@ -25,7 +25,6 @@ class ChatManager:
         else:
             return response
 
-
     # Sends a message to everyong in the channel
     def sendMessageToChat(self, message):
         self.s.send("PRIVMSG {} :{}\r\n".format(cfg.CHAN, message).encode("utf-8"))
@@ -34,6 +33,7 @@ class ChatManager:
     def sendMessageToChatServer(self):
         self.s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
 
+    # TODO: ban a user
     def ban(user):
         """
         Ban a user from the current channel.
@@ -43,6 +43,7 @@ class ChatManager:
         """
         self.sendMessageToChat(".ban {}".format(user))
 
+    # TODO: timeout a user
     def timeout(user, secs=600):
         """
         Time out a user for a set period of time.
@@ -52,8 +53,3 @@ class ChatManager:
         secs -- the length of the timeout in seconds (default 600)
         """
         self.sendMessageToChat(".timeout {}".format(user, secs))
-
-
-
-
-
